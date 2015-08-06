c871197b42b9d0becf0d6651eda13aca61602d1d
Dawid Ciężarkiewicz <dpc@dpc.pw>
2015-08-06 00:14:15 -0700

    Long chain test.

--- src/tests.rs
+++ src/tests.rs
@@ -1,6 +1,10 @@
 use super::start;
 
+use std;
+use std::io::{Read, Write};
 use std::sync::{Arc, Mutex};
+
+use mio;
 
 #[test]
 fn empty_handler() {
@@ -67,6 +71,57 @@ fn contain_panics_in_subcoroutines() {
             mioco.spawn(|_| {
                 panic!()
             });
+        }
+
+        let mut lock = finished_copy.lock().unwrap();
+        *lock = true;
+
+        Ok(())
+    });
+
+    assert!(*finished_ok.lock().unwrap());
+}
+
+#[test]
+fn long_chain() {
+    let finished_ok = Arc::new(Mutex::new(false));
+
+    let finished_copy = finished_ok.clone();
+    start(move |mioco| {
+
+        let (first_reader, first_writer) = try!(mio::unix::pipe());
+
+        let mut prev_reader = first_reader;
+
+        // TODO: increase after https://github.com/dpc/mioco/issues/8 is fixed
+        for _ in 0..128 {
+            let (reader, writer) = try!(mio::unix::pipe());
+
+            mioco.spawn(move |mioco| {
+                let mut reader = mioco.wrap(prev_reader);
+                let mut writer = mioco.wrap(writer);
+
+                let _ = std::io::copy(&mut reader, &mut writer);
+
+                Ok(())
+            });
+
+            prev_reader = reader;
+        }
+
+        let mut first_writer = mioco.wrap(first_writer);
+        let mut last_reader = mioco.wrap(prev_reader);
+
+        for i in 0..9 {
+            let test_str = format!("TeSt{}", i);
+            let _ = first_writer.write_all(test_str.as_str().as_bytes());
+            let mut buf = [0u8; 16];
+
+            let _ = last_reader.read(&mut buf);
+
+            if &buf[0..5] != test_str.as_str().as_bytes() {
+                panic!();
+            }
         }
 
         let mut lock = finished_copy.lock().unwrap();
