5dc0dd7fe51a3e4bac52ce9c5336180d83c6a6a0
Dawid Ciężarkiewicz <dpc@dpc.pw>
2015-08-10 20:57:35 -0800

    Update documentation

--- implementors/mio/handler/trait.Handler.js
+++ implementors/mio/handler/trait.Handler.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['mioco'] = ["impl <a class='trait' href='mio/handler/trait.Handler.html' title='mio::handler::Handler'>Handler</a> for <a class='struct' href='mioco/struct.Server.html' title='mioco::Server'>Server</a>",];
+implementors['mioco'] = ["impl <a class='trait' href='mio/handler/trait.Handler.html' title='mio::handler::Handler'>Handler</a> for <a class='struct' href='mioco/struct.Handler.html' title='mioco::Handler'>Handler</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
