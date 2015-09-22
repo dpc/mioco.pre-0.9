ed99a0b234ae2477fa16ef415679d0825eccb778
Dawid Ciężarkiewicz <dpc@dpc.pw>
2015-09-21 23:36:19 -0700

    Make UDP example multiport.

--- examples/echo_udp.rs
+++ examples/echo_udp.rs
@@ -2,35 +2,36 @@ extern crate mio;
 extern crate mioco;
 extern crate env_logger;
 
-use std::net::SocketAddr;
-use std::str::FromStr;
-
+use std::net::{SocketAddr, SocketAddrV4};
 use mio::buf::{MutSliceBuf, SliceBuf};
 use mio::udp::{UdpSocket};
-const DEFAULT_LISTEN_ADDR : &'static str = "127.0.0.1:5555";
+use mio::Ipv4Addr;
 
-fn listend_addr() -> SocketAddr {
-    FromStr::from_str(DEFAULT_LISTEN_ADDR).unwrap()
-}
+const START_PORT : u16 = 60000;
+const END_PORT   : u16 = 65535;
 
 fn main() {
     env_logger::init().unwrap();
 
     mioco::start(move |mioco| {
-        let addr = listend_addr();
+        println!("Starting udp echo server on ports: {}-{}", START_PORT, END_PORT);
 
-        let sock = try!(UdpSocket::v4());
-        try!(sock.bind(&addr));
+        for port in START_PORT..END_PORT {
+            mioco.spawn(move |mioco| {
+                let ip = Ipv4Addr::new(0, 0, 0, 0);
+                let addr = SocketAddr::V4(SocketAddrV4::new(ip, port));
 
-        println!("Starting udp echo server on {:?}", sock.local_addr().unwrap());
+                let sock = try!(UdpSocket::v4());
+                try!(sock.bind(&addr));
+                let mut sock = mioco.wrap(sock);
 
-        let mut sock = mioco.wrap(sock);
-
-        let mut buf = [0u8; 1024 * 16];
-
-        loop {
-            let addr = try!(sock.read(&mut MutSliceBuf::wrap(&mut buf)));
-            try!(sock.write(&mut SliceBuf::wrap(&buf), &addr));
+                let mut buf = [0u8; 1024 * 16];
+                loop {
+                    let addr = try!(sock.read(&mut MutSliceBuf::wrap(&mut buf)));
+                    try!(sock.write(&mut SliceBuf::wrap(&buf), &addr));
+                }
+            });
         }
+        Ok(())
     });
 }
