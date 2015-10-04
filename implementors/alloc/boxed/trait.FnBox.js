3f71be59df23f5a777c40f7480dba22d90b1513f
Dawid Ciężarkiewicz <dpc@dpc.pw>
2015-10-03 16:42:57 -0800

    Update documentation

--- implementors/alloc/boxed/trait.FnBox.js
+++ implementors/alloc/boxed/trait.FnBox.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['log'] = [];implementors['regex_syntax'] = [];implementors['mio'] = [];implementors['mioco'] = [];implementors['mioco'] = [];implementors['mioco'] = [];implementors['mioco'] = [];implementors['mioco'] = [];implementors['mioco'] = [];
+implementors['log'] = [];implementors['regex_syntax'] = [];implementors['mio'] = [];implementors['mioco'] = [];implementors['mioco'] = [];implementors['mioco'] = [];implementors['mioco'] = [];implementors['mioco'] = [];implementors['mioco'] = [];implementors['mioco'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
