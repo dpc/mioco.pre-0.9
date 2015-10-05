3fac5529fa65704b4b8871071320e4b61a2c657b
Dawid Ciężarkiewicz <dpc@dpc.pw>
2015-10-04 21:48:35 -0800

    Update documentation

--- implementors/core/any/trait.Any.js
+++ implementors/core/any/trait.Any.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['mio'] = [];implementors['mioco'] = [];implementors['mioco'] = [];
+implementors['mio'] = [];implementors['mioco'] = [];implementors['mioco'] = [];implementors['mioco'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
