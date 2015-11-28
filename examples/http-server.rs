29a93ae56bb5e812f9ab6652327f0eda745d09d1
Dawid Ciężarkiewicz <dpc@dpc.pw>
2015-11-27 22:59:46 -0800

    Add `http-server.rs` example.

---
+++ examples/http-server.rs
@@ -0,0 +1,90 @@
+extern crate mioco;
+extern crate env_logger;
+extern crate httparse;
+
+use std::net::SocketAddr;
+use std::str::FromStr;
+use std::io::{Write, Read};
+use mioco::mio::tcp::{TcpSocket};
+
+const DEFAULT_LISTEN_ADDR : &'static str = "127.0.0.1:5555";
+
+fn listend_addr() -> SocketAddr {
+    FromStr::from_str(DEFAULT_LISTEN_ADDR).unwrap()
+}
+
+const RESPONSE: &'static str = "HTTP/1.1 200 OK\r
+Content-Length: 14\r
+\r
+Hello World\r
+\r";
+
+const RESPONSE_404: &'static str = "HTTP/1.1 404 Not Found\r
+Content-Length: 14\r
+\r
+Hello World\r
+\r";
+
+
+fn main() {
+    env_logger::init().unwrap();
+    let addr = listend_addr();
+
+    let sock = TcpSocket::v4().unwrap();
+    sock.bind(&addr).unwrap();
+    let sock = sock.listen(1024).unwrap();
+
+    println!("Starting mioco http server on {:?}", sock.local_addr().unwrap());
+
+    mioco::start(move |mioco| {
+        for _ in 0..mioco.thread_num() {
+            let sock = try!(sock.try_clone());
+            mioco.spawn(move |mioco| {
+
+                let sock = mioco.wrap(sock);
+
+                loop {
+                    let conn = try!(sock.accept());
+                    mioco.spawn(move |mioco| {
+                        let mut conn = mioco.wrap(conn);
+
+                        let mut buf_i = 0;
+                        let mut buf = [0u8; 1024];
+
+                        let mut headers = [httparse::EMPTY_HEADER; 16];
+                        loop {
+                            let len = try!(conn.read(&mut buf[buf_i..]));
+
+                            if len == 0 {
+                                return Ok(());
+                            }
+
+                            buf_i += len;
+
+                            let mut req = httparse::Request::new(&mut headers);
+                            let res = req.parse(&buf[0..buf_i]).unwrap();
+
+                            if res.is_complete() {
+                                let req_len = res.unwrap();
+                                match req.path {
+                                    Some(ref _path) => {
+                                        let _ = try!(conn.write_all(&RESPONSE.as_bytes()));
+                                        if req_len != buf_i {
+                                            // request has a body; TODO: handle it
+                                        }
+                                        buf_i = 0;
+                                    },
+                                    None => {
+                                        let _ = try!(conn.write_all(&RESPONSE_404.as_bytes()));
+                                        return Ok(());
+                                    }
+                                }
+                            }
+                        }
+                    });
+                }
+            });
+        }
+        Ok(())
+    });
+}
