8db0c1007dbf2a6dd325a13c71caa6ba68629202
Dawid Ciężarkiewicz <dpc@dpc.pw>
2016-03-01 23:02:26 -0800

    Update documentation

--- implementors/core/slice/bytes/trait.MutableByteVector.js
+++ implementors/core/slice/bytes/trait.MutableByteVector.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];implementors['mio'] = [];implementors['mioco'] = [];implementors['mioco'] = [];implementors['mioco'] = [];
+implementors['libc'] = [];implementors['mio'] = [];implementors['mioco'] = [];implementors['mioco'] = [];implementors['mioco'] = [];implementors['libc'] = [];implementors['mio'] = [];implementors['mioco'] = [];implementors['mioco'] = [];implementors['mioco'] = [];implementors['mioco'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
