9f46bf5db6cc730ea547146bac5867c679cee9be
kteza1 <k.teza1@gmail.com>
2016-07-15 16:57:39 +0530

    updating channel_sync example

--- examples/channel_sync.rs
+++ examples/channel_sync.rs
@@ -2,26 +2,22 @@
 extern crate mioco;
 extern crate env_logger;
 
-use std::net::SocketAddr;
-use std::str::FromStr;
-use mioco::tcp::TcpListener;
-use mioco::Mioco;
-use mioco::sync::mpsc::Receiver;
 use std::thread;
-use std::io::{self, Read, Write, BufRead};
-
-const DEFAULT_LISTEN_ADDR : &'static str = "127.0.0.1:5555";
+use std::io::{self, BufRead};
+use std::time::Duration;
 
 fn main() {
     env_logger::init().unwrap();
-    let (mail_send, mail_recv) = mioco::sync::mpsc::sync_channel::<i32>(5);  
+    let (mail_send, mail_recv) = mioco::sync::mpsc::sync_channel::<String>(5);  
 
     thread::spawn(move|| {
         loop {
+            println!("Enter something:");
             let stdin = io::stdin();
             let mut line = String::new();
-            stdin.lock().read_line(&mut line).unwrap();
-            let _ = mail_send.send(0);
+            stdin.lock().read_line(&mut line).ok().expect("Failed to read line");
+            let line = line.parse::<String>().expect("Not a number");
+            let _ = mail_send.send(line);
         }
     });
 
@@ -31,9 +27,10 @@ fn main() {
                     timer.set_timeout(5000);
                     select!(
                         r:timer => {
-                            println!("TIMEELL");
+                            println!("TIMEOUT");
                         },
                         r:mail_recv => {
+                            thread::sleep(Duration::new(2, 0));
                             let message = mail_recv.recv();
                             println!("{:?}", message); 
                         },
